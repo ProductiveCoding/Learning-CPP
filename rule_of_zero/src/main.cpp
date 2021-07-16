24cb240fe55ec0a13f662dffd73ca8bc1da009bd
Stefano Sandolo <stefano.sandolo@gmail.com>
2021-07-16 08:33:53 +0200

    Update main.cpp
    Source code formatting and comments updating

--- rule_of_zero/src/main.cpp
+++ rule_of_zero/src/main.cpp
@@ -16,22 +16,22 @@ Widget f(Widget w)
 int main(int argc, char *argv[])
 {
 	std::cout << "Widget w1" << std::endl;
-    Widget w1;						// default construct
+	Widget w1;						// Call default construct
 
-std::cout << "\nWidget w3{w1}" << std::endl;
-    Widget w3{ w1 };				// copy constructor
-
-std::cout << "\nWidget w4 = w3" << std::endl;
-	Widget w4 = w3;					// copy constructor
+	std::cout << "\nWidget w3{w1}" << std::endl;
+	Widget w3{ w1 };					// Call copy constructor
+	
+	std::cout << "\nWidget w4 = w3" << std::endl;
+	Widget w4 = w3;						// Call copy constructor
 
 	std::cout << "w3 = w1" << std::endl;
-	w3 = w1;						// Assignement operator
+	w3 = w1;						// Call assignement operator
 
 	std::cout << "\nWidget w5 = std::move(w3)" << std::endl;
-    Widget w5 = std::move(w3);      // Move constructor
+	Widget w5 = std::move(w3);      			// Call move constructor
 
 	std::cout << "\nw5 = std::move(w1)" << std::endl;
-    w5 = std::move(w1);				// Move Assignemnt
+	w5 = std::move(w1);					// Call move Assignemnt
 
 	std::cout << "\nWidget w6 = f(w5)" << std::endl;
 	Widget w6 = f(w5);
